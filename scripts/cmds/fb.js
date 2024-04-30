8437316786d89824657670838551ab6197203005
Sahaduu <158268018+Sahaduu@users.noreply.github.com>
2024-04-30 14:01:11 +0600

    Update fb.js

--- scripts/cmds/fb.js
+++ scripts/cmds/fb.js
@@ -1,43 +1,45 @@
 const axios = require("axios");
 
 module.exports = {
-  config: {
-    name: "fb",
-aliases: ["fbdl","videofb"],
-    version: 1.0,
-    author: "OtinXSandip",
-    longDescription: "Download Facebook reel",
-    role: 0,
-    category: "media",
-    guide: {
-      en: "{p}{n} link",
+    config: {
+        name: "fb",
+        aliases: ["fbdl","videofb","fbvid"],
+        version: 1.0,
+        author: "TCA",
+        longDescription: "Download videos from Facebook",
+        role: 0,
+        category: "media",
+        guide: {
+            en: "{p}{n} [link]",
+        },
     },
-  },
-  onStart: async function ({ message, usersData, event, api, args }) {
-    try {
-      const url = args.join(" ");
-      if (!url) {
-        return message.reply("Please provide a link.");
-      }
+    onStart: async function ({ message, args }) {
+        try {
+            const url = args.join(" ");
+            if (!url) {
+                return message.reply("Please provide a link.");
+            }
 
-      const downloadUrl = `https://sandipbaruwal.onrender.com/download?url=${url}`;
-      const response = await axios.get(downloadUrl);
-      const hdMedia = response.data.medias.find(media => media.quality === 'hd');
+            const downloadUrl = `https://sandipbaruwal.onrender.com/download?url=${url}`;
+            const response = await axios.get(downloadUrl);
+            const hdMedia = response.data.medias.find(media => media.quality === 'hd');
 
-      if (!hdMedia) {
-        return message.reply("No HD media found.");
-      }
+            if (!hdMedia) {
+                return message.reply("No HD media found.");
+            }
 
-      const shortUrl = await require('tinyurl').shorten(hdMedia.url);
-      const stream = await global.utils.getStreamFromURL(hdMedia.url);
+            const shortUrl = await require('tinyurl').shorten(hdMedia.url);
+            const stream = await global.utils.getStreamFromURL(hdMedia.url);
 
-      message.reply({ 
-        body: shortUrl,
-        attachment: stream
-      });
-    } catch (error) {
-      console.error("Error:", error.message);
-      message.reply({ body: "❌ An error occurred" });
+            const caption = `(Unknown Content)\n\nQuality: 720p(HD)\nHere's the link: ${shortUrl}`;
+
+            message.reply({ 
+                body: caption,
+                attachment: stream
+            });
+        } catch (error) {
+            console.error("Error:", error.message);
+            message.reply({ body: "❌ An error occurred" });
+        }
     }
-  }
 };
